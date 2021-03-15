ad410d94b80b19c024e86115568ba4e204ff6a20
masayoshi sato <masayoshi418979@icloud.com>
2021-03-16 00:45:00 +0900

    changed file

--- Linear_Regression_for_Prediction.R
+++ Linear_Regression_for_Prediction.R
@@ -92,3 +92,74 @@ rmse <-function(n){
 
 set.seed(1, sample.kind="Rounding")
 sapply(n, rmse)
+
+#Q3
+set.seed(1, sample.kind="Rounding") 
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.95, 0.95, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+
+
+set.seed(1, sample.kind="Rounding")
+
+RMSE_100 <- replicate(n,{
+  test_index <- createDataPartition(dat$y, p= 0.5, list = F,times = 1)
+  train_set <- dat %>% slice(-test_index)
+  test_set <-dat %>% slice(test_index)
+  fit <- lm(y~x, data=train_set)
+  y_hat <-predict(fit, test_set)
+  RMSE(test_set$y, y_hat)
+})
+
+mean(RMSE_100)
+sd(RMSE_100)
+
+#Q6
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+
+
+#answers method    
+
+  set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  
+  fit <- lm(y ~ x_1, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+  
+  fit <- lm(y ~ x_2, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+  
+  fit <- lm(y ~ x_1 + x_2, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+  
+#Q8
+  set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+  Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+  dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+    data.frame() %>% setNames(c("y", "x_1", "x_2"))
+  
+  set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  
+  fit <- lm(y ~ x_1, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+  
+  fit <- lm(y ~ x_2, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+  
+  fit <- lm(y ~ x_1 + x_2, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2)) 
