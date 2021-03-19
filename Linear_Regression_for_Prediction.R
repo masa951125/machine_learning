c91b97f25ec8006413f1545c5a97244039b3c59f
masayoshi sato <masayoshi418979@icloud.com>
2021-03-20 00:37:49 +0900

    changed file and new plot

--- Linear_Regression_for_Prediction.R
+++ Linear_Regression_for_Prediction.R
@@ -241,3 +241,45 @@ dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
   
 read_mnist()
 
+#Q1
+set.seed(2, sample.kind="Rounding") #if you are using R 3.6 or later
+make_data <- function(n = 1000, p = 0.5, 
+                      mu_0 = 0, mu_1 = 2, 
+                      sigma_0 = 1,  sigma_1 = 1){
+  
+  y <- rbinom(n, 1, p)
+  f_0 <- rnorm(n, mu_0, sigma_0)
+  f_1 <- rnorm(n, mu_1, sigma_1)
+  x <- ifelse(y == 1, f_1, f_0)
+  
+  test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+  
+  list(train = data.frame(x = x, y = as.factor(y)) %>% slice(-test_index),
+       test = data.frame(x = x, y = as.factor(y)) %>% slice(test_index))
+}
+dat <- make_data()
+
+
+
+set.seed(1, sample.kind="Rounding")
+mu_1 <- seq(0, 3, len=25)
+
+
+fit_glm <-dat$train %>% glm(y~x, family="binomial",data=.)
+p_hat_glm <- predict(fit_glm, dat$test)
+y_hat_glm <- factor(ifelse(p_hat_glm>0.5, 1,0))
+confusionMatrix(data = y_hat_glm, reference = dat$test$y)$overall["Accuracy"]
+
+###
+set.seed(1, sample.kind="Rounding")
+delta <- seq(0, 3, len=25)
+
+res <- sapply(delta, function(d){
+  dat <- make_data(mu_1 =d)
+  fit_glm <-  glm(y~x, family="binomial",data=dat$train)
+  p_hat_glm <- predict(fit_glm, dat$test)
+  y_hat_glm <- ifelse(p_hat_glm>0.5, 1, 0) %>% factor(c(1,0))
+  mean(y_hat_glm== dat$test$y)
+})
+
+qplot(mu_1, res)
