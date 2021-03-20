c9c94575bf76ca0e9bcf4acefc1d38ac70814baa
masayoshi sato <masayoshi418979@icloud.com>
2021-03-21 01:06:56 +0900

    file changed

--- Smoothing.R
+++ Smoothing.R
@@ -23,3 +23,85 @@ polls_2008 %>% mutate(smooth = fit$y) %>%
 polls_2008 %>% ggplot(aes(day, margin)) +
   geom_point() + 
   geom_smooth(color="red", span = 0.15, method = "loess", method.args = list(degree=1))
+
+#comprehension check
+
+library(tidyverse)
+library(lubridate)
+library(purrr)
+install.packages("pdftools")
+library(pdftools)
+
+fn <- system.file("extdata", "RD-Mortality-Report_2015-18-180531.pdf", package="dslabs")
+dat <- map_df(str_split(pdf_text(fn), "\n"), function(s){
+  s <- str_trim(s)
+  header_index <- str_which(s, "2015")[1]
+  tmp <- str_split(s[header_index], "\\s+", simplify = TRUE)
+  month <- tmp[1]
+  header <- tmp[-1]
+  tail_index  <- str_which(s, "Total")
+  n <- str_count(s, "\\d+")
+  out <- c(1:header_index, which(n==1), which(n>=28), tail_index:length(s))
+  s[-out] %>%
+    str_remove_all("[^\\d\\s]") %>%
+    str_trim() %>%
+    str_split_fixed("\\s+", n = 6) %>%
+    .[,1:5] %>%
+    as_data_frame() %>% 
+    setNames(c("day", header)) %>%
+    mutate(month = month,
+           day = as.numeric(day)) %>%
+    gather(year, deaths, -c(day, month)) %>%
+    mutate(deaths = as.numeric(deaths))
+}) %>%
+  mutate(month = recode(month, "JAN" = 1, "FEB" = 2, "MAR" = 3, "APR" = 4, "MAY" = 5, "JUN" = 6, 
+                        "JUL" = 7, "AGO" = 8, "SEP" = 9, "OCT" = 10, "NOV" = 11, "DEC" = 12)) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  dplyr::filter(date <= "2018-05-01")
+
+#how to find span
+total_days <- diff(range(dat$date))%>%as.numeric
+span <- 60/total_days
+
+
+#plot smooth
+dat %>% ggplot(aes(date, deaths)) +
+  geom_point() + 
+  geom_smooth(color="red", span = span, method = "loess", method.args = list(degree=1))
+
+#answer
+span <- 60 / as.numeric(diff(range(dat$date)))
+fit <- dat %>% mutate(x = as.numeric(date)) %>% loess(deaths ~ x, data = ., span = span, degree = 1)
+dat %>% mutate(smooth = predict(fit, as.numeric(date))) %>%
+  ggplot() +
+  geom_point(aes(date, deaths)) +
+  geom_line(aes(date, smooth), lwd = 2, col = "red")
+
+dat %>% 
+  mutate(smooth = predict(fit, as.numeric(date)), day = yday(date), year = as.character(year(date))) %>%
+  ggplot(aes(day, smooth, col = year)) +
+  geom_line(lwd = 2)
+
+#Q3
+data("mnist_27")
+mnist_27
+library(broom)
+mnist_27$train %>% glm(y ~ x_2, family = "binomial", data = .) %>% tidy()
+qplot(x_2, y, data = mnist_27$train)
+str(mnist_27)
+range(mnist_27$train$x_1)
+
+mnist_27$train %>%
+  mutate(y = ifelse(y==7, 1, 0)) %>%
+  lm(y ~ x_2, data = .)
+
+fit <- mnist_27$train %>%  mutate(y = ifelse(y==7, 1, 0)) %>% loess(y ~ x_2, data = ., span = 0.5, degree = 1)
+mnist_27$train %>% mutate(y = ifelse(y==7, 1, 0),smooth = predict(fit, x_2)) %>%
+  ggplot() +
+  geom_point(aes(x_2, y)) +
+  geom_line(aes(x_2, smooth), lwd = 2, col = "red")
+
+mnist_27$train %>% 
+  mutate(y = ifelse(y=="7", 1, 0)) %>%
+  ggplot(aes(x_2, y)) + 
+  geom_smooth(method = "loess")
